4690ecf14d2a8a2001bb7739e3132b2ebbb7ec96
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-09-05 20:03:12 +0100

    fix: compilation error from missing dependency

--- server/src/resolvers/hello.ts
+++ server/src/resolvers/hello.ts
@@ -5,6 +5,6 @@ import { Query, Resolver } from "type-graphql"
 export class HelloResolver {
     @Query(() => String)
     hello() {
-        return "Hello world"
+        return "Hello world, this is Mircea"
     }
 }
