91a290cc910b99c787e5f16cd9c0cb1d15002a3c
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-09-10 13:51:51 +0100

    feat: Add submit button

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Form, Formik } from "formik"
-import { Box} from "@chakra-ui/core";
+import { Box, Button } from "@chakra-ui/core";
 import Wrapper from "../components/Wrapper"
 import { InputField } from "../components/InputField"
 
@@ -17,7 +17,7 @@ const Register: React.FC<registerProps> = ({}) => {
                     console.log(values)
                 } }
             >
-                { ({ values, handleChange }) => (
+                { ({isSubmitting}) => (
                     <Form>
                         <InputField
                             label="Username"
@@ -32,6 +32,7 @@ const Register: React.FC<registerProps> = ({}) => {
                                 type="password"
                             />
                         </Box>
+                        <Button mt={4} isLoading={isSubmitting} variantColor="teal" type="submit">register</Button>
                     </Form>
                 ) }
             </Formik>
