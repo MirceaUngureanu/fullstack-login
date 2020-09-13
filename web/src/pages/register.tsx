14369cb2cea50b8ce960d42a03d0c330d8da278d
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-09-13 17:47:35 +0100

    feat: Add Graphql-codegen plugin for custom hooks

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -3,32 +3,15 @@ import { Form, Formik } from "formik"
 import { Box, Button } from "@chakra-ui/core";
 import Wrapper from "../components/Wrapper"
 import { InputField } from "../components/InputField"
-import { useMutation } from "urql";
+import { useRegisterMutation } from "../generated/graphql";
 
 interface registerProps {
 
 }
 
-const REGISTER_MUT = `
-    mutation Register($username: String!, $password: String!) {
-      register(options:{
-        username: $username,
-        password: $password
-      }) {
-        errors {
-          field
-          message
-        }
-        user {
-          id
-          username
-        }
-      }
-    }
-`
 
 const Register: React.FC<registerProps> = ({}) => {
-    const [ {}, register ] = useMutation(REGISTER_MUT)
+    const [ {}, register ] = useRegisterMutation()
 
     return (
         <Wrapper variant="small">
