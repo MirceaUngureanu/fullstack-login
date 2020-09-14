efcd78aabf1fe852b8d96d1a4678bd8c5de26716
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-09-14 12:48:41 +0100

    refactor: Add fragment for user queries

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -38,7 +38,7 @@ export const NavBar: React.FC<{}> = ({}) => {
 
     return (
         <Flex
-            bg="tomato"
+            bg="tan"
             p={ 4 }>
             <Box ml={ "auto" }>
                 { body }
