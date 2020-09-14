ebd8b27c79b38a0db21a4720c496c77f38f7bd2c
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-09-14 12:26:09 +0100

    feat: Add logic to navbar to account for state

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Box, Flex, Link } from "@chakra-ui/core";
+import { Box, Button, Flex, Link } from "@chakra-ui/core";
 import NextLink from "next/link"
+import { useMeQuery } from "../generated/graphql";
 
 export const NavBar: React.FC<{}> = ({}) => {
-    return (
-        <Flex bg="tomato" p={4}>
-            <Box ml={"auto"}>
+    const [ { data, fetching } ] = useMeQuery()
+    let body = null
+
+    // data is loading
+    if (fetching) {
+
+        // user is not logged in
+    } else if (!data?.me) {
+        body = (
+            <>
                 <NextLink href="/login">
                     <Link mr="2">
                         login
@@ -16,6 +24,24 @@ export const NavBar: React.FC<{}> = ({}) => {
                         register
                     </Link>
                 </NextLink>
+            </>
+        )
+        // use is logged in
+    } else {
+        body =(
+            <Flex>
+                <Box mr={2}>{data.me.username}</Box>
+                <Button variant="link">logout</Button>
+            </Flex>
+        )
+    }
+
+    return (
+        <Flex
+            bg="tomato"
+            p={ 4 }>
+            <Box ml={ "auto" }>
+                { body }
             </Box>
         </Flex>
     )
