7c4721dd1d5bb3ad5d9c93c1cc631a8be30eda2b
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-09-21 19:14:39 +0100

    feat: Add nodemailer dependency to server

--- server/src/constant.ts
+++ server/src/constant.ts
@@ -1,2 +1,6 @@
 export const __prod__ = process.env.NODE_ENV === 'production'
 export const COOKIE_NAME = "qid"
+
+// if nodemailer test user and pass expire use the createTestAccount function in "sendEmail.ts" to generate a new one
+export const TEST_NODEMAILER_USE = ***REMOVED***
+export const TEST_NODEMAILER_PASS = ***REMOVED***
